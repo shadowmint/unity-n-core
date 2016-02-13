9dc8ea2914bdc209c2e7548d1ace020c9a38c0d0
Douglas Linder <linderd@iinet.net.au>
2016-02-13 16:06:35 +0800

    update version, remove events

--- src/n-core/engine/utils/_.cs
+++ src/n-core/engine/utils/_.cs
@@ -1,26 +1,23 @@
-namespace N
+/// Magical utility class~
+public class _
 {
-    /// Magical utility class~
-    public class _
+    /// Return a new exception
+    public static System.Exception Error(string format, params object[] args)
     {
-        /// Return a new exception
-        public static System.Exception Error(string format, params object[] args)
-        {
-            var msg = string.Format(format, args);
-            return new System.Exception(msg);
-        }
+        var msg = string.Format(format, args);
+        return new System.Exception(msg);
+    }
 
-        /// Log a message
-        public static void Log(string format, params object[] args)
-        {
-            var msg = string.Format(format, args);
-            N.Console.Log(msg);
-        }
+    /// Log a message
+    public static void Log(string format, params object[] args)
+    {
+        var msg = string.Format(format, args);
+        N.Console.Log(msg);
+    }
 
-        /// Log a message
-        public static void Log(object target)
-        {
-            N.Console.Log(target);
-        }
+    /// Log a message
+    public static void Log(object target)
+    {
+        N.Console.Log(target);
     }
 }
